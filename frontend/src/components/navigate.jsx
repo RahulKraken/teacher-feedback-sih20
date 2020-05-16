0f4e4f3f4df43f54cdca094b72878ebb2639510d
angel112 <ngllakra@gmail.com>
2020-05-16 17:17:56 +0530

    auth ui complete

--- frontend/src/components/navigate.jsx
+++ frontend/src/components/navigate.jsx
@@ -6,16 +6,20 @@ class Navigate extends Component {
   state = {};
   render() {
     return (
-      <Navbar bg="dark" variant="dark" collapseOnSelect>
+      <Navbar
+        bg="dark"
+        variant="dark"
+        collapseOnSelect
+        style={{ fontFamily: "Montserrat" }}
+      >
         <div className="container">
           <Navbar.Brand>
-            <a to="/">SIH20</a>
+            <a href="/">SIH20</a>
           </Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
             <Nav pullRight>
-              <Nav.Link href="/auth/login">Login</Nav.Link>
-              <Nav.Link href="/auth/signup">Signup</Nav.Link>
+              <Nav.Link href="/auth/login">Login/Signup</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </div>
--- frontend/src/components/signupForm.jsx
+++ frontend/src/components/signupForm.jsx
@@ -3,13 +3,27 @@ import { Form, Button } from "react-bootstrap";
 import "../styles/form.css";
 
 const SignupForm = () => {
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
     const data = new FormData(e.target);
 
     const name = data.get("name");
     const email = data.get("email");
     const pwd = data.get("pwd");
+    const url = "http://localhost:5000/signup";
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_name: name,
+        email: email,
+        pasword: pwd,
+      }),
+    };
+    const response = await fetch(url, options);
+    console.log(response.status);
   }
   return (
     <Form className="loginWindow" onSubmit={handleSubmit}>
@@ -42,6 +56,12 @@ const SignupForm = () => {
           placeholder="Enter Password"
           required
         />
+      </Form.Group>
+      <Form.Group controlID="passwordForm">
+        <input type="checkbox" id="terms" name="terms" required />
+        <label className="lbl terms" for="terms">
+          I accept the Terms and Conditions
+        </label>
       </Form.Group>
       <Button type="submit">Signup</Button>
     </Form>
